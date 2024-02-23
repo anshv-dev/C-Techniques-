41b8a6ef0e8ef3c1bd73ee938e044d776cff8361
Ansh <anshv8166@gmail.com>
2024-02-23 20:19:38 +0530

    V-2

---
+++ BinarySearch.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n=8;
+int arr[n]={3,4,5,8,9,10,12,15};
+int target=9;
+int low=0;
+int high=n-1;
+while(low<=high){
+  int mid=(low+high)/2;
+  if(arr[mid]==target){
+    cout<<"Target found"<<" "<<mid;
+    break;
+  }else if(target>arr[mid]){
+    low=mid+1;
+  }else{
+    high=mid-1;
+  }
+}
+return 0;
+}
